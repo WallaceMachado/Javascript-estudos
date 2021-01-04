7bccf2886e6b473bdee86984dfcb47ad346434c9
wallacemachado <wallacefm18@gmail.com>
2021-01-04 19:37:58 -0300

    array

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function entrar (){
-    var area = document.getElementById('area');
-    var texto = prompt('Digite seu nome');
 
-    area.innerHTML = texto;
-}
+
+var lista = ["wallace","lucia","Fran"];
+console.log(lista);
+console.log(lista.length);
+console.log(lista[1]);
+
+//verificando s tem elemento no array
+console.log(lista.indexOf("wallace"));
+
+//verificando s tem elemento no array retorno será -1
+console.log(lista.indexOf("wallaceaaa"));
+
+//concatenar elementos de 1 array
+console.log(lista.join(", "));
+console.log(lista.join("/"));
+
+//remover ultimo elemento
+console.log(lista.pop());
+console.log(lista);
+
+//remover primeiro elemento
+console.log(lista.shift());
+console.log(lista);
+
+//alterar elemento de um index
+lista[0]="matheus";
+console.log(lista);
+
+//adicionar
+
+lista.push("wallace","fran");
+console.log(lista);
+
+
+
+
+
+
+
