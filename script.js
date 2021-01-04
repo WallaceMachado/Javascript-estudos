30269813c0173c5176ce868c950d3e59165d4990
wallacemachado <wallacefm18@gmail.com>
2021-01-04 19:51:09 -0300

    laço for e while

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
 
 
 var lista = ["wallace","lucia","Fran"];
-console.log(lista);
-console.log(lista.length);
-console.log(lista[1]);
+var x=0;
+while(x < 10) {
+    document.write("</br> o valor é: " + x);
+    x++;
 
-//verificando s tem elemento no array
-console.log(lista.indexOf("wallace"));
+}
 
-//verificando s tem elemento no array retorno será -1
-console.log(lista.indexOf("wallaceaaa"));
-
-//concatenar elementos de 1 array
-console.log(lista.join(", "));
-console.log(lista.join("/"));
-
-//remover ultimo elemento
-console.log(lista.pop());
-console.log(lista);
-
-//remover primeiro elemento
-console.log(lista.shift());
-console.log(lista);
-
-//alterar elemento de um index
-lista[0]="matheus";
-console.log(lista);
-
-//adicionar
-
-lista.push("wallace","fran");
-console.log(lista);
+for (a = 0; a<10; a++){
+    document.write("</br> o valor é: " + a);
+     
+}
 
 
 
