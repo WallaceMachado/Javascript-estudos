c05d399a95ecc93e91edaeb215b04880363129ea
wallacemachado <wallacefm18@gmail.com>
2021-01-05 08:13:04 -0300

    let e var

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-/* Cookies são arquivos criados pelos websites que você visita.
- Eles tornam sua experiência on-line mais fácil, 
- economizando informações de navegação. 
- Com os cookies, os sites podem manter você conectado, 
- lembrar suas preferências do site e fornecer conteúdo 
- relevante localmente. pode guardar ultima pagina acessada e senhas, te manter logado
+var lista = [1,2,3];
 
- Cookie é um mecanismo de armazenamento de suas variáveis do lado do cliente. É armazenado fisicamente no computador do cliente pelo navegador. Diferentes usuários no mesmo computador podem ler/utilizar o mesmo cookie.
+for(var i in lista){
+    console.log(lista[i]);
+}
 
-Por conta disso (algumas observações):
+for(let x in lista){
+    console.log(lista[x]);
+}
 
-Você não deve armazenar dados sensíveis no cookie.
-Você não deve armazenar dados que pertence a uma conta de usuário.
-O cookie não tem efeito sobre os recursos do servidor.
-Cookie expira na data que você especificar.
-A Session é também um mecanismo de armazenamento de suas variáveis, porém do lado do servidor. Por padrão, a session armazena os seus dados na memória do servidor. Mas você pode configurar para armazenar pelo SQL Server, por exemplo. O mesmo usuário pode executar dois ou mais navegadores e cada navegador tem a sua própria sessão.
+console.log(i);
+console.log(x);// não é impresso pq o let só existe dentro do contexto que foi criado
 
-Ou seja:
-
-Você pode guardar dados sensíveis em sessão.
-Você não deve salvar tudo em sessão. que é desperdício de recursos do servidor.
-Após o usuário fechar o navegador, o timeout fará com que todas as informações sejam apagadas. Por padrão, esse tempo é de 20 minuto
-
-*/
-
-// localstrore = cookie ou seja mesmo ele fechand o navegador o dado fica salvo
-
-localStorage.setItem("nome", "matheus");
-console.log(localStorage.getItem("nome"));
-
-//outra forma de fazer
-localStorage.sobrenome="machado";
-console.log(localStorage.sobrenome);
-
-
-//localStorage.removeItem("nome");
