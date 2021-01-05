fb7d8ece3c53cce7819997393bac14b57ec9ec21
wallacemachado <wallacefm18@gmail.com>
2021-01-05 08:39:11 -0300

    desconstruindo array e objeto

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let nome = "leo";
-let sobrenome ="machado";
+let nomes = ["leo","machado",10];
+let[nome]=nomes;
+console.log(nome);
 
-let pessoa = "meu nome é:" + nome + " " + sobrenome;
-console.log(pessoa);
+let[nome1,nome2,idade]=nomes;
+console.log(nome1,nome2,idade);
 
-let pessoa2 = `meu nome é: ${nome} ${sobrenome}`;
-console.log(pessoa2);
+let{0:primeironome, 1:segundonome}=nomes;
+console.log(primeironome,segundonome);
+
+const pessoa0 = {
+    nome0: "sara",
+    sobrenome0:"silva",
+    idade0:10
+};
+
+let{nome0,idade0} = pessoa0;
+console.log(nome0,idade0);
+
+let{nome0:novonome} = pessoa0;
+console.log(novonome);
+
+
