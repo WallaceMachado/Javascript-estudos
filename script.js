bf9c9d7f5b5411fb442d4c75026575c852fc20dd
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-07-16 12:25:33 -0300

    pecorrendo um objeto

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-function notasEscolares(nota){
-    if(nota>=90){
-            return 'A'
-    }else if (nota>=80 && nota<=89){
-            return 'B'
-    } else if(nota>=70 && nota<=79){
-            return 'C'
-    }else if (nota>=60 && nota<=69){
-            return 'D'
-    }else{
-            return 'F'
-            
-    }
+let person = {
+    x: {
+        name: 'wallace',
+        age: 30,
+        weight: 88.6
+    },
+    y: {
+        name: 'wallace',
+        age: 30,
+        weight: 88.6
+    },
+
 }
-
-let nota = notasEscolares(80)
-
-console.log(nota);
-
-// receitas e despetas
-
-let total;
-const gastos  = {
-    receitas: [10,500, 1000],
-    despesas: [10,400,680],
+for (let property in person) {
+    console.log(property)
+    console.log(person[property].name)
+    person[property].name='trocado';
+    console.log(person[property].name)
 }
-
-for (let property in gastos){
-    let totalReceitas=0;
-    let receitas = gastos["receitas"]
-    for(let receita of receitas ){
-        totalReceitas += receita
-    }
-    let totalDespesas=0;
-    despesas = gastos["despesas"]
-    for(let despesa of despesas ){
-        totalDespesas += despesa
-    }
-
-    total = totalReceitas- totalDespesas
-}
-
-console.log(total)
